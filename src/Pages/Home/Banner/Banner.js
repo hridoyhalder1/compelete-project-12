80cd4d40ac4c5160cf087bb5abcae623c3d0ed36
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-11-24 11:05:47 +0600

    footer added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
+            <div className="hero min-h-screen mb-14">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
                     <div>

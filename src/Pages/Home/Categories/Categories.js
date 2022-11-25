0d7dbb6ccbe37214adf19239469fc1397ccbdfb3
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-11-25 13:55:35 +0600

    category data load from server

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -5,7 +5,7 @@ const Categories = () => {
 
     const [categories, setCategories] = useState([]);
     useEffect( () => {
-        fetch('categoryy.json')
+        fetch('http://localhost:5000/category')
         .then(res => res.json())
         .then(data => setCategories(data))
     }, [])
@@ -13,7 +13,7 @@ const Categories = () => {
     return (
         <div className='mb-12'>
             <h2 className="text-4xl font-bold text-center my-16 ">Products Category</h2>
-            <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto'>
                 {
                     categories.map(category => <CategoryCard
                     key={category._id}

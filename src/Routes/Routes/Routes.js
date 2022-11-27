bedb6b20d7b723883c464a4b694b2a397e3ae182
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-11-27 23:09:50 +0600

    my orders table

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,14 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../../layout/DashboardLayout";
 import Main from "../../layout/Main";
 import Blog from "../../Pages/Blog/Blog";
 import Dashboard from "../../Pages/Dashboard/Dashboard";
+import MyOrder from "../../Pages/Dashboard/MyOrder/MyOrder";
 import Products from "../../Pages/Home/Categories/Products";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/Login/SignUp";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
- const router = createBrowserRouter([
+const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
@@ -32,24 +34,32 @@ import PrivateRoute from "../PrivateRoute/PrivateRoute";
             {
                 path: '/products/:id',
                 element: <Products></Products>,
-                loader: ({params}) => fetch(`https://final-assignment-server-site.vercel.app/products/${params.id}`)
-            },
+                loader: ({ params }) => fetch(`https://final-assignment-server-site.vercel.app/products/${params.id}`)
+            }
+
+        ]
+    },
+    {
+        path: '/dashboard',
+        element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
+        children: [
             {
                 path: '/dashboard',
-                element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>
-            }
-            
+                element: <MyOrder></MyOrder>
+            },
+
         ]
     },
     {
         path: '*',
         element: <div>
-            <h2 className="text-4xl text-center mt-40 font-bold">404 Page<br/>
-            Page Not Found
+            <h2 className="text-4xl text-center mt-40 font-bold">404 Page<br />
+                Page Not Found
             </h2>
 
         </div>
     }
+    
 ])
 
 export default router;

961e03ea3ecb29f17e251719152db922c9c649d8
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-11-28 20:33:27 +0600

    some update

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -19,7 +19,6 @@ const Navbar = () => {
 
     const menuItems = <>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>About</Link></li>
         <li><Link to='/blog'>Blog</Link></li>
 
         {
